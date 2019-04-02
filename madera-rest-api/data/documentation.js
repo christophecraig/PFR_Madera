f19f4d9c8cd3efc7240bf2f3f355ce81a67abd5d
Christophe Craig <christophe.craig@viacesi.fr>
2019-04-02 11:50:17 +0200

    delete documentation -- useless

--- madera-rest-api/data/documentation.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../mysqlConnexion').connection
-
-module.exports = {
-  get(req, res, next) {
-    connection.query('describe ?', req.params.table, (err, results, fields) => {
-      res.json(results)
-    })
-  }
-}
