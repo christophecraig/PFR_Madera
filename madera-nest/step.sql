3397468fcfd79bf5484bb27166824f3f17e1afa6
Christophe Craig <christophe.craig@viacesi.fr>
2019-05-20 16:24:15 +0200

    step data

---
+++ madera-nest/step.sql
@@ -0,0 +1,9 @@
+INSERT INTO `step` (`name`, `completion`) VALUES
+('A la signature', 3),
+('Obtention du permis de construire', 10),
+('Ouverture du chantier', 15),
+('Achèvement des fondations', 25),
+('Achèvement des murs', 40),
+('Mise hors d\'eau / hors d\'air', 75),
+('Achèvement des travaux d\'équipement (plomberie, menuiserie, chauffage)', 95),
+('Remise des clés', 100);
